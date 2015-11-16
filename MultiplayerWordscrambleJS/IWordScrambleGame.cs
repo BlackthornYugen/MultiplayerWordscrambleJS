8e9a242652e451df6e5170e2e20930d5eb288b40
John Steel <john@steelcomputers.com>
2015-11-16 09:20:24 -0500

    Init(ish)

---
+++ MultiplayerWordscrambleJS/IWordScrambleGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MultiplayerWordscrambleJS
+{
+	[ServiceContract]
+	public interface IWordScrambleGame
+	{
+		// Returns true if the game is already being hosted or false otherwise 
+		[OperationContract]
+		bool IsGameBeingHosted();
+
+		// User ‘userName’ tries to host the game with word ‘wordToScramble’
+		// The function returns the name of the person hosting the game 
+		// Exception: game is already being hosted by someone else
+		[OperationContract]
+        string HostGame(string playerName, string hostAddress, string wordToScramble);
+
+		// Player ‘playerName’ tries to join the game
+		// The function returns a Word object containing the host’s (un)scrambled words
+		// Exception: maximum number of players reached
+		// Exception: host cannot join the game
+		// Exception: nobody is hosting the game
+		[OperationContract]
+		Word Join(string playerName);
+
+		// Player ‘playerName’ guesses word ‘guessedWord’ compared with word ‘unscrambledWord’
+		// Returns true if ‘guessedWord’ is identical to ‘unscrambledWord’ or false otherwise
+		// The function returns the name of the person hosting the game 
+		// Exception: user is not playing the game 
+		[OperationContract]
+		bool GuessWord(string playerName, string guessedWord, string unscrambledWord);
+	}
+
+	[DataContract]
+	public class Word
+	{
+		[DataMember]
+		public string UnscrambledWord; // word typed by the game’s host
+		[DataMember]
+		public string ScrambledWord;
+	}
+}
---
+++ MultiplayerWordscrambleJS/WordScrambleGame.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MultiplayerWordscrambleJS
+{
+    [ServiceBehavior]
+    public class WordScrambleGame : IWordScrambleGame
+    {
+        // the maximum number of players allowed playing simultaneously
+        private const int MAX_PLAYERS = 5;
+        // the user hosting the game. If it’s null nobody is hosting the game.
+        private static string userHostingTheGame = null;
+        // the Word object that contains the scrambled and unscrambled words
+        private static Word gameWords;
+        // the list of players playing the game
+        private static List<string> activePlayers = new List<string>();
+
+        [OperationBehavior]
+        public bool IsGameBeingHosted()
+        {
+            return userHostingTheGame != null;
+        }
+
+        [OperationBehavior]
+        public string HostGame(string playerName, string hostAddress, string wordToScramble)
+        {
+            if (IsGameBeingHosted())
+            {
+                throw new Exception();
+            }
+
+            return ScrambleWord(wordToScramble);
+        }
+
+        [OperationBehavior]
+        public Word Join(string playerName)
+        {
+            // TO BE COMPLETED BY YOU: Add exception and program logic
+            throw new NotImplementedException();
+        }
+
+        [OperationBehavior]
+        public bool GuessWord(string playerName, string guessedWord, string unscrambledWord)
+        {
+            // TO BE COMPLETED BY YOU: Add exception and program logic
+            throw new NotImplementedException();
+        }
+
+        // Utility function to scramble a word
+        private string ScrambleWord(string word)
+        {
+            char[] chars = word.ToArray();
+            Random r = new Random(2011);
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int randomIndex = r.Next(0, chars.Length);
+                char temp = chars[randomIndex];
+                chars[randomIndex] = chars[i];
+                chars[i] = temp;
+            }
+            return new string(chars);
+        }
+    }
+
+}
